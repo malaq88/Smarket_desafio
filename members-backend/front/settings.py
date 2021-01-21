e879e59ccd3b606910a8b0787ec4ad67b7e1189f
Malaq <atrito12@gmail.com>
2021-01-21 00:41:00 -0300

    integrando primeira api de frontend e dando acesso ao links externos acessar o servidor backend

--- members-backend/front/settings.py
+++ members-backend/front/settings.py
@@ -37,11 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # external apps
     'rest_framework',
+    'corsheaders',
+    # apps
     'core',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:4200"
+]
--- members-frontend/src/app/app.component.ts
+++ members-frontend/src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'members-frontend';
+  title = 'members-front';
+
+  members = [
+    {name: 'Member 01', id: 1, surname: 'ciclano', photo: 'http://www.minhaap.com/photo.png'},
+    {name: 'Member 02', id: 2, surname: 'ciclano', photo: 'http://www.minhaap.com/photo.png'},
+    {name: 'Member 03', id: 3, surname: 'ciclano', photo: 'http://www.minhaap.com/photo.png'},
+  ];
+
+  constructor(private api:ApiService) {
+    this.getMembers();
+  }
+
+  getMembers = () => {
+    this.api.getAllMembers().subscribe(
+      data => {
+        this.members = data
+      },
+      error => {
+        console.log("Aconteceu um erro");
+      }
+    );
+  };
 }
